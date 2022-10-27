f65559dcbbbeb9358c545c34353244b63159237f
jey25 <jjangeoyoung@gmail.com>
2022-10-28 00:25:20 +0900

    Create goldmetal.cs

---
+++ goldmetal.cs
@@ -0,0 +1,187 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goldmetal : MonoBehaviour
+{
+
+    // void TestFunction()
+    // {
+    //     print("test1");
+    //     print("test2");
+    // }
+    void Start()
+    {
+        // for (int i = 0; i < 5; i++)
+        // {
+        //     TestFunction();
+        // }
+
+        // Debug.Log("Hello Unity");
+
+        //1. 변수
+        // int level = 5;
+        // float strength = 15.5f;
+        // string playerName = "장어영";
+        // bool isFullLevel = false;
+
+        // Debug.Log(playerName);
+        // Debug.Log(level);
+        // Debug.Log(strength);
+        // Debug.Log(isFullLevel);
+
+        //2. 그룹형 변수
+        // string[] monsters = { "슬라임", "사막뱀", "악마" };
+
+        // Debug.Log("몬스터");
+        // Debug.Log(monsters[0]);
+        // Debug.Log(monsters[1]);
+        // Debug.Log(monsters[2]);
+
+        // int[] monsterLevel = new int[3];
+        // monsterLevel[0] = 1;
+        // monsterLevel[1] = 6;
+        // monsterLevel[2] = 20;
+
+        // Debug.Log(monsterLevel[0]);
+        // Debug.Log(monsterLevel[1]);
+        // Debug.Log(monsterLevel[2]);
+
+        // List<string> items = new List<string>();
+        // items.Add("생명물약20");
+        // items.Add("마나물약20");
+
+        // //0번 아이템을 지운다
+        // items.RemoveAt(0);
+
+        // Debug.Log(items[0]);
+        // Debug.Log(items[1]);
+
+
+        //3. 연산자
+        // int exp = 0;
+
+        // exp = 1500 + 320;
+        // exp = exp - 10;
+        // int level = exp / 300;
+        // float strength = level * 3.1f;
+
+        // Debug.Log(exp);
+        // Debug.Log(level);
+        // Debug.Log(strength);
+
+        // int nextExp = 300 - (exp % 300);
+        // Debug.Log(nextExp);
+
+        // string title = "전설의";
+        // Debug.Log(title + " " + playerName);
+
+        // int fullLevel = 99;
+        // isFullLevel = level == fullLevel;
+        // Debug.Log(isFullLevel);
+
+        // bool isEndTutorial = level > 10;
+        // Debug.Log(isEndTutorial);
+
+        // int health = 30;
+        // int mana = 25;
+
+        // int num;
+        // num = 10;
+        // print(num);
+
+        // int a = 2;
+        // int b = 5;
+        // int temp;
+        // temp = a;
+        // a = b;
+        // b = temp;
+        // print(a);
+        // print(b);
+
+
+        //자동 형변환
+        // float fNum1;
+        // fNum1 = 2;  //2가 저장됐지만 2.0 으로 출력
+        // print(fNum1);
+
+        // float fNum2;
+        // fNum2 = 2.0f; //2.0 이 저장된 상태
+        // print(fNum2);
+
+        //강제 형변환
+        // int intNum;
+        // intNum = (int)2.1f; //int 가 되었기에 소수점 자리는 사라짐
+
+        // int intNum = 2;
+        // float fNum;
+        // fNum = intNum; //2.0 으로 저장됨
+        // print(fNum);
+        // print(intNum);
+
+        // int a;
+        // float b;
+        // float c;
+        // a = 2;
+        // b = 0.1f;
+        // c = a + b;
+        // print(c);
+
+        // int a;
+        // float b;
+        // int c;
+        // a = 3;
+        // b = 0.1f;
+        // c = (int)(a + b);
+        // print(c);
+
+        // int num1, num2, num3, sum;
+        // float avr;
+        // num1 = 5;
+        // num2 = 20;
+        // num3 = 30;
+        // sum = num1 + num2 + num3;
+        // avr = (float)sum / 3;
+        // print(avr);
+
+        // int a = 3;
+        // print(a);
+        // a++;
+        // print(a);
+        // a += 5;
+        // print(a);
+        // a -= 2;
+        // print(a);
+        // a = a + 1;
+        // print(a);
+
+        //4. 반복문
+
+        // int i;
+        // for (i = 0; i < 3; i++)
+        // {
+        //     print("hi");
+        // }
+
+        // int i;
+        // for (i = 1; i < 10; i++)
+        // {
+        //     print("2 * " + i + " " + "= " + 2 * i);
+        // }
+
+        // void MyPrint(int a)
+        // {
+        //     int num = 1;
+        //     print(a + num);
+        // }
+        // MyPrint(5);
+
+
+        //4. 키워드
+        // int float string bool new List
+
+
+
+    }
+
+}
